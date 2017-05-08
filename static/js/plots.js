e6a68948be843f191165a04bf0b25798602298ce
Enrico Vianello <vianello94@gmail.com>
2017-05-08 18:00:46 +0200

    Switched from smoothie.js to chart.js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,65 +1,80 @@
-// Random data, just for testing purposes
-var temp = new TimeSeries();
-var hum = new TimeSeries();
-var brightness = new TimeSeries();
-setInterval(function() {
-	// retrieve last measure and append to collection of measures
-	var last_request = new XMLHttpRequest()
-	last_request.open("GET", "/last_datum", false);
-	last_request.send();
-
-	var measure = JSON.parse(last_request.response);
-
-	temp.append(new Date().getTime(), measure.temperature);
-	hum.append(new Date().getTime(), measure.humidity);
-	brightness.append(new Date().getTime(), measure.brightness);
-}, 1000);
-
-function on_request_success(response) {
-	console.debug('response', response);
-}
-
-function on_request_error(r, text_status, error_thrown) {
-	console.debug('error', text_status + ", " + error_thrown + ":\n" + r.responseText);
-}
-
 function createPlots(){
-	var smoothie = new SmoothieChart({
-		grid: { strokeStyle: 'rgb(125, 0, 0)',
-		fillStyle: 'rgb(60, 0, 0)',
-		lineWidth: 1,
-		millisPerLine: 250,
-		verticalSections: 6,
-		responsive: true} });
-	smoothie.addTimeSeries(temp, {
-		strokeStyle: 'rgb(0, 255, 0)',
-		fillStyle: 'rgba(0, 255, 0, 0.4)',
-		lineWidth: 3 });
-	temp1_canvas = document.getElementById("temp1");
-	fitToContainer(temp1_canvas);
-	smoothie.streamTo(temp1_canvas, 1000);
+	var canvas = document.getElementById('temp-canvas'),
+	    ctx = canvas.getContext('2d'),
+	    startingData = {
+	      labels: [1, 2, 3, 4, 5, 6, 7],
+	      datasets: [
+	          {
+	              fillColor: "rgba(220,220,220,0.2)",
+	              strokeColor: "rgba(220,220,220,1)",
+	              pointColor: "rgba(220,220,220,1)",
+	              pointStrokeColor: "#fff",
+	              data: [65, 59, 80, 81, 56, 55, 40]
+	          },
+	          {
+	              fillColor: "rgba(151,187,205,0.2)",
+	              strokeColor: "rgba(151,187,205,1)",
+	              pointColor: "rgba(151,187,205,1)",
+	              pointStrokeColor: "#fff",
+	              data: [28, 48, 40, 19, 86, 27, 90]
+	          }
+	      ]
+	    },
+	    latestLabel = startingData.labels[6];
 
-	smoothie = new SmoothieChart({
-		grid: { strokeStyle: 'rgb(125, 0, 0)',
-		fillStyle: 'rgb(60, 0, 0)',
-		lineWidth: 1,
-		millisPerLine: 250,
-		verticalSections: 6,
-		responsive: true} });
-	smoothie.addTimeSeries(hum, {
-		strokeStyle: 'rgb(140, 0, 120)',
-		fillStyle: 'rgba(140, 0, 120, 0.4)',
-		lineWidth: 3 });
-	hum1_canvas = document.getElementById("hum1");
-	fitToContainer(hum1_canvas);
-	smoothie.streamTo(hum1_canvas, 1000);
+	// Reduce the animation steps for demo clarity.
+	var myLiveChart = new Chart(ctx).Line(startingData, {animationSteps: 15});
 }
 
 
-/* Make a canvas visually fill the positioned parent object */
-function fitToContainer(canvas){
-	canvas.style.width ='100%';
-	canvas.style.height='100%';
-	canvas.width	= canvas.offsetWidth;
-	canvas.height = canvas.offsetHeight;
-}
+setInterval(function(){
+  // Add two random numbers for each dataset
+  myLiveChart.addData([Math.random() * 100, Math.random() * 100], ++latestLabel);
+  // Remove the first point so we dont just add values forever
+  myLiveChart.removeData();
+}, 5000);
+
+
+
+
+
+// setInterval(function() {
+// 	// retrieve last measure and append to collection of measures
+// 	var last_request = new XMLHttpRequest()
+// 	last_request.open("GET", "/last_datum", false);
+// 	last_request.send();
+//
+// 	var measure = JSON.parse(last_request.response);
+//
+// 	// Add two random numbers for each dataset
+//     myLiveChart.addData([measure.temperature], ++latestLabel);
+//     // Remove the first point so we dont just add values forever
+//     myLiveChart.removeData();
+// }, 1000);
+//
+// var canvas = document.getElementById('temp1'),
+//     ctx = canvas.getContext('2d'),
+//     startingData = {
+//       labels: [0,0,0,0,0,0,0,0,0],
+//       datasets: [
+//           {
+//               fillColor: "rgba(220,220,220,0.2)",
+//               strokeColor: "rgba(220,220,220,1)",
+//               pointColor: "rgba(220,220,220,1)",
+//               pointStrokeColor: "#fff",
+//               data: [65, 59, 80, 81, 56, 55, 40]
+//           },
+//           {
+//               fillColor: "rgba(151,187,205,0.2)",
+//               strokeColor: "rgba(151,187,205,1)",
+//               pointColor: "rgba(151,187,205,1)",
+//               pointStrokeColor: "#fff",
+//               data: [28, 48, 40, 19, 86, 27, 90]
+//           }
+//       ]
+//     },
+//     latestLabel = startingData.labels[8];
+//
+// // Reduce the animation steps for demo clarity.
+// var myLiveChart = new Chart(ctx).Line(startingData, {animationSteps: 15});
+// fitToContainer(temp1_canvas);
