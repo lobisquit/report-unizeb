94371c43b777df65be3d5a8cf40ebb5b6d433c5a
Enrico Vianello <vianello94@gmail.com>
2017-05-10 23:20:44 +0200

    Last things fixed (hopefully)

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,55 +1,91 @@
 var NUMBER_OF_POINTS = 15;
+var ANIMATION_SECONDS = 2;
+
+
+Chart.defaults.global.animationSteps = Math.round(ANIMATION_SECONDS * 1000 / 17);
 
 function createPlots(){
 	var canvas
 	canvas = document.getElementById('temp-canvas'),
-	    temp_ctx = canvas.getContext('2d'),
-	    temp_startingData = {
-	      labels: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0),// new Array(20),
-	      datasets: [
-	          {
-	              fillColor: "rgba(220,220,220,0.2)",
-	              strokeColor: "rgba(220,220,220,1)",
-	              pointColor: "rgba(220,220,220,1)",
-	              pointStrokeColor: "#fff",
-	                data: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0)
-	          }
-	      ]
-	    };
+		temp_ctx = canvas.getContext('2d'),
+		temp_startingData = {
+			labels: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0),
+			datasets: [
+				{
+					fillColor: "rgba(237, 72, 47,0.2)",
+					strokeColor: "rgba(220,220,220,1)",
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					data: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0)
+				}
+			],
+			title: {
+				display: true,
+				text: 'Custom Chart Title'
+			}
+		};
+
+	var temp_options = {
+		title: {
+			display: true,
+			text: 'Custom Chart Title',
+			position: 'left'
+		},
+		legend: {
+			display: true,
+			text: 'prova',
+			labels: {
+					fontColor: 'rgb(255, 99, 132)'
+			}
+		},
+	}
+
 	canvas = document.getElementById('hum-canvas'),
-	    hum_ctx = canvas.getContext('2d'),
-	    hum_startingData = {
-	      labels: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0),// new Array(20),
-	      datasets: [
-	          {
-	              fillColor: "rgba(220,220,220,0.2)",
-	              strokeColor: "rgba(220,220,220,1)",
-	              pointColor: "rgba(220,220,220,1)",
-	              pointStrokeColor: "#fff",
-	                data: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0)
-	          }
-	      ]
-	    };
+		hum_ctx = canvas.getContext('2d'),
+		hum_startingData = {
+			labels: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0),
+			datasets: [
+				{
+					fillColor: "rgba(58, 162, 232,0.2)",
+					strokeColor: "rgba(220,220,220,1)",
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					data: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0)
+				}
+			],
+			options: {
+				title: {
+					display: true,
+					text: 'Custom Chart Title'
+				}
+			}
+		};
 	canvas = document.getElementById('lum-canvas'),
-	    lum_ctx = canvas.getContext('2d'),
-	    lum_startingData = {
-	      labels: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0),// new Array(20),
-	      datasets: [
-	          {
-	              fillColor: "rgba(220,220,220,0.2)",
-	              strokeColor: "rgba(220,220,220,1)",
-	              pointColor: "rgba(220,220,220,1)",
-	              pointStrokeColor: "#fff",
-	                data: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0)
-	          }
-	      ]
-	    };
+		lum_ctx = canvas.getContext('2d'),
+		lum_startingData = {
+			labels: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0),
+			datasets: [
+				{
+					fillColor: "rgba(34, 139, 34,0.2)",
+					strokeColor: "rgba(220,220,220,1)",
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					data: Array.apply(null, Array(NUMBER_OF_POINTS)).map(Number.prototype.valueOf,0)
+				}
+			],
+			options: {
+				title: {
+					display: true,
+					text: 'Custom Chart Title'
+				}
+			}
+		};
 
 
 	// Reduce the animation steps for demo clarity.
-	var temp_chart = new Chart(temp_ctx).Line(temp_startingData, {animationSteps: 15});
-	var hum_chart = new Chart(hum_ctx).Line(hum_startingData, {animationSteps: 15});
-	var lum_chart = new Chart(lum_ctx).Line(lum_startingData, {animationSteps: 15});
+	var temp_chart = new Chart(temp_ctx).Line(temp_startingData, temp_options);
+	var hum_chart = new Chart(hum_ctx).Line(hum_startingData);
+	var lum_chart = new Chart(lum_ctx).Line(lum_startingData);
 
 
 	var prevTime = undefined;
